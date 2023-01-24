d0b195e947602e06187c6ffaf5e39530d0259bd7
Pannoniae <prisznyak.zsombor@gmail.com>
2023-01-24 18:10:59 +0000

    - Fixed hello world template

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,7 @@
 package com.napier.sem;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Boo yah!");
     }
 }
