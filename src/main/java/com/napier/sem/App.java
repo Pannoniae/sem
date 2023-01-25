0870c118db602e612512c2f500afea4b9f0a2ae5
ervette <kiriltalalayko@gmail.com>
2023-01-25 19:02:39 +0000

    first try!

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,6 @@ package com.napier.sem;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Boo yah!");
+        System.out.println("Boo yah!!");
     }
 }
